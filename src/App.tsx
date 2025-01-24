8b7e09308e257efa01467dc4c0ba3750facca6b8
Alex Lougheed <lougheedalex@gmail.com>
2025-01-24 17:28:43 +0000

    Removed possibly redundant router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 import Navbar from './components/Navbar'
 import { Home } from './pages/Home'
 
 const App = () => {
   return (
     <div className="min-h-screen bg-primaryTeal">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Home />
     </div>
   )
 }
