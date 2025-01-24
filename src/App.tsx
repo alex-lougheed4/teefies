42cd571addd14c9231a21e8a63a7364bbf6e2f1d
Alex Lougheed <lougheedalex@gmail.com>
2025-01-24 17:01:46 +0000

    Removed redundant routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
-import { About } from './pages/About'
-import { Contact } from './pages/Contact'
 import { Home } from './pages/Home'
-import { Services } from './pages/Services'
 
 const App = () => {
   return (
@@ -13,9 +10,6 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
